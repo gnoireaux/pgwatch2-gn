3dddf101581763fc42b0efdc57d4c967df7a1f72
Guillaume Noireaux <gnoireaux@gmail.com>
2024-09-03 11:25:37 +0200

    add metrics db to grafana config.

--- pgwatch2/bootstrap/grafana_datasource_pg.sql
+++ pgwatch2/bootstrap/grafana_datasource_pg.sql
@@ -1,6 +1,19 @@
 insert into data_source (org_id, version, type, name, access, url,
   password, "user", database, basic_auth, is_default, json_data, created, updated
   ) values (
-  1, 0, 'postgres', 'pg-metrics', 'proxy', 'localhost:5432',
-  'pgwatch2admin', 'pgwatch2', 'pgwatch2_metrics', 'f', 't', '{"postgresVersion":1000,"sslmode":"disable","timescaledb":false}', now(), now()
+               1,
+               0,
+               'postgres',
+               'pg-metrics',
+               'proxy',
+               'be7ulq1gnwd665rd9ocd-postgresql.services.clever-cloud.com:6784',
+               'cshhjjuwxhnpasnrdrmlwfjsdj7wkx',
+               'urogrzedhmauvoyiulcd',
+               'be7ulq1gnwd665rd9ocd',
+               'f',
+               't',
+               '{"postgresVersion":1550,"sslmode":"disable","timescaledb":true}',
+               now(),
+               now()
+
   );
